0d0261293e1149d7f3555917ecef04c3b85559af
thaidmfinnick <thai.dm279@gmail.com>
2025-08-20 20:52:06 +0700

    feat: bump libmpv to 0.39.0
    build with libmpv-darwin-build

--- media_kit_test/macos/Flutter/GeneratedPluginRegistrant.swift
+++ media_kit_test/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import media_kit_libs_macos_video
 import media_kit_video
 import package_info_plus
@@ -12,6 +13,7 @@ import path_provider_foundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
